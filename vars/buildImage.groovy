8ead7dd6bf6f550c2f1dc0a4a54ee838ba7e6f3d
Anmol <anmolkhilwani@gmail.com>
2023-03-30 19:03:16 +0530

    edit

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,7 @@
 def call() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
+    echo "Building the Docker Image"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'pass', usernameVariable: 'user')]){
+        sh 'docker build -t anmol0503/docker-prac:2.0 .'
+        sh "echo $pass | docker login -u $user --password-stdin"
+        sh 'docker push anmol0503/docker-prac:2.0'
 }
