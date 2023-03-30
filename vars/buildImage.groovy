8aea33d316577fc6a3f64890477b856b3955c7ba
Anmol <anmolkhilwani@gmail.com>
2023-03-30 23:34:51 +0530

    edit

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,5 @@
+import com.example.Docker
+
 def call(String imageName) {
-    echo "Building the Docker Image"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'pass', usernameVariable: 'user')]){
-        sh "docker build -t $imageName ."
-        sh "echo $pass | docker login -u $user --password-stdin"
-        sh "docker push $imageName"
-    }
+    return new Deocker(this).buildDockerImage(imageName)
 }
